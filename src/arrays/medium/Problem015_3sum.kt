20d6b93c62ff2a4482fca41ad92d0270f56cc65d
TruongVimIT <truongvimit@gmail.com>
2025-11-03 19:53:32 +0700

    ✨ 015 draft

---
+++ src/arrays/medium/Problem015_3sum.kt
@@ -0,0 +1,27 @@
+/**
+ * Problem 15: 3sum
+ * https://leetcode.com/problems/3sum/
+ * medium | TODO: Add tags
+ *
+ * TODO: Add description
+ * Time: O(?), Space: O(?)
+ */
+
+package arrays.medium
+
+class Problem015_3sum {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+
+    }
+}
+
+// Test directly in problem file
+fun main() {
+    val solution = Problem015_3sum()
+
+    // Test cases
+    println("=== Problem 15: 3sum ===")
+    // TODO: Add test cases
+    // solution.solve()
+    println("✅ Tests completed!")
+}
