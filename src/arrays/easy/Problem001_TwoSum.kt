e717c8f84832457a004e75fef55bf543e909309f
TruongVimIT <truongvimit@gmail.com>
2025-11-11 11:19:54 +0700

    Update 001

--- src/arrays/easy/Problem001_TwoSum.kt
+++ src/arrays/easy/Problem001_TwoSum.kt
@@ -13,16 +13,13 @@ package arrays.easy
 class Problem001_TwoSum {
 
     fun twoSum(nums: IntArray, target: Int): IntArray{
-        val n = nums.size
-        val intArray = IntArray(2)
-        for (i in 0 until n) {
-            for (j in (i+1) until n) {
-                if((nums[i]+nums[j]) == target){
-                    intArray[0] = i
-                    intArray[1] = j
-                    return intArray
-                }
+        val mapNeeded = hashMapOf<Int, Int>()
+        for (i in 0 until nums.size){
+            val needed = target - nums[i]
+            if (mapNeeded.containsKey(needed)){
+                return intArrayOf(mapNeeded.getValue(needed), i)
             }
+            mapNeeded[nums[i]] = i
         }
         throw IllegalArgumentException("No Solution")
     }
@@ -35,5 +32,5 @@ fun main() {
     // Test cases
     println("Test 1: ${solution.twoSum(intArrayOf(2, 7, 11, 15), 9).contentToString()}") // [0,1]
     println("Test 2: ${solution.twoSum(intArrayOf(3, 2, 4), 6).contentToString()}")     // [1,2]
-    println("Test 3: ${solution.twoSum(intArrayOf(3, 3), 6).contentToString()}")       // [0,1]
+    println("Test 3: ${solution.twoSum(intArrayOf(3, 2, 4, 1, 5, 2), 6).contentToString()}")       // [0,1]
 }
